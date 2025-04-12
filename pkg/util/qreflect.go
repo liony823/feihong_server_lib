dceaf79b85399a4279f4e65eea08821127fd6907
lion <yanyahonglong@gmail.com>
2025-04-12 15:30:36 +0800

    feat: 新增 IsZeroValue 和 CopyStructFields 函数
    新增 IsZeroValue 函数用于检查值是否为其类型的零值，新增 CopyStructFields 函数用于将结构体字段复制到 map 中，增强了工具包的功能。

--- pkg/util/qreflect.go
+++ pkg/util/qreflect.go
@@ -20,3 +20,60 @@ func AttrToUnderscore(st interface{}) []string {
 	}
 	return names
 }
+
+// IsZeroValue 检查一个值是否为其类型的零值
+func IsZeroValue(v interface{}) bool {
+	if v == nil {
+		return true
+	}
+
+	val := reflect.ValueOf(v)
+
+	switch val.Kind() {
+	case reflect.String:
+		return val.Len() == 0
+	case reflect.Bool:
+		return !val.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return val.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return val.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return val.Float() == 0
+	case reflect.Slice, reflect.Map, reflect.Array:
+		return val.Len() == 0
+	case reflect.Ptr, reflect.Interface:
+		return val.IsNil()
+	default:
+		return reflect.DeepEqual(v, reflect.Zero(val.Type()).Interface())
+	}
+}
+
+// CopyStructFields 将结构体字段复制到map中
+func CopyStructFields(dest *map[string]interface{}, src interface{}) error {
+	if dest == nil {
+		return nil
+	}
+
+	v := reflect.ValueOf(src)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+
+	if v.Kind() != reflect.Struct {
+		return nil
+	}
+
+	t := v.Type()
+	for i := 0; i < v.NumField(); i++ {
+		field := t.Field(i)
+		if field.PkgPath != "" {
+			continue // 跳过非导出字段
+		}
+
+		fieldValue := v.Field(i).Interface()
+		(*dest)[field.Name] = fieldValue
+	}
+
+	return nil
+}
